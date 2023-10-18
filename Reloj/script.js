d20618bf7784038124fd3f78b0a738cae5180cbe
JuanRodriguez1219 <110642261+JuanRodriguez1219@users.noreply.github.com>
2023-10-17 20:08:17 -0500

    Add files via upload

---
+++ Reloj/script.js
@@ -0,0 +1,18 @@
+const deg = 6;
+
+const horas = document.querySelector('.horas');
+const minutos = document.querySelector('.minutos');
+const segundos = document.querySelector('.segundos');
+
+setInterval(()=>{
+    let tiempo = new Date();
+
+
+    let hr = tiempo.getHours()*30;
+    let min = tiempo.getMinutes()*deg;
+    let seg = tiempo.getSeconds()*deg;
+
+    horas.style.transform = `rotate(${(hr)+(min/12)}deg)`;
+    minutos.style.transform = `rotate(${min}deg)`;
+    segundos.style.transform = `rotate(${seg}deg)`;
+})
